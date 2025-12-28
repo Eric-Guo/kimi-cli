625aa819e3b3af38f0f8a67ed6bd636212f5c246
Eric-Guo <eric.guocz@gmail.com>
2025-12-28 20:24:22 +0800

    Longer timeout.

--- src/kimi_cli/tools/shell/__init__.py
+++ src/kimi_cli/tools/shell/__init__.py
@@ -14,7 +14,7 @@ from kimi_cli.tools.utils import ToolRejectedError, ToolResultBuilder, load_desc
 from kimi_cli.utils.environment import Environment
 from kimi_cli.utils.subprocess_env import get_clean_env
 
-MAX_TIMEOUT = 5 * 60
+MAX_TIMEOUT = 10 * 60
 
 
 class Params(BaseModel):
@@ -24,7 +24,7 @@ class Params(BaseModel):
             "The timeout in seconds for the command to execute. "
             "If the command takes longer than this, it will be killed."
         ),
-        default=60,
+        default=600,
         ge=1,
         le=MAX_TIMEOUT,
     )
